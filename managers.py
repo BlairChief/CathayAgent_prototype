3cd65f369a2883dd680a19f333f1c5df272f3aec
Bijon Setyawan Raya <bijonsetyawan@gmail.com>
2024-12-19 15:52:29 +0800

    feat: pdf knowledge base manager

---
+++ managers.py
@@ -0,0 +1,35 @@
+
+from shared import DB_URL
+from phi.vectordb.pgvector import PgVector
+from phi.knowledge.pdf import PDFKnowledgeBase, PDFReader
+
+class KnowledgeBaseManager:
+    def __init__(
+        self, 
+        source_path="data/pdfs", 
+        table_name="pdfs", 
+        db_url=DB_URL, 
+        chunk=True
+    ):
+        self.source_path = source_path
+        self.table_name = table_name
+        self.db_url = db_url
+        self.chunk = chunk
+        self.pdf_knowledge_base = None
+
+    def initialize_knowledge_base(self):
+        vector_db = PgVector(
+            table_name=self.table_name,
+            db_url=self.db_url
+        )
+        reader = PDFReader(chunk=self.chunk)
+        self.pdf_knowledge_base = PDFKnowledgeBase(
+            path=self.source_path,
+            vector_db=vector_db,
+            reader=reader
+        )
+
+    def get_knowledge_base(self):
+        if self.pdf_knowledge_base is None:
+            self.initialize_knowledge_base()
+        return self.pdf_knowledge_base
