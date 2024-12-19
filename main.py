b2a041de097d37e1b18c9621a7b1cdb147966a9f
Bijon Setyawan Raya <bijonsetyawan@gmail.com>
2024-12-19 15:52:13 +0800

    feat: initialize knowledge base

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 import streamlit as st
-from agents import WebAgent
+from agents import WebAgent, RetrievalAgent
+from managers import KnowledgeBaseManager
 
 web_agent = WebAgent()
+
+knowledge_base = KnowledgeBaseManager()
+knowledge_base.initialize_knowledge_base()
+
+# knowledge_base.pdf_knowledge_base.load(recreate=True, upsert=True)
+
+retrieval_agent = RetrievalAgent(knowledge_base.pdf_knowledge_base)
 
 if 'openai_model' not in st.session_state:
     st.session_state['openai_model'] = 'gpt-4o'
@@ -25,8 +33,10 @@ if prompt := st.chat_input('What is up?'):
         st.markdown(prompt)
 
     with st.chat_message('assistant'):
-        stream=web_agent.ask(prompt)
+        stream=retrieval_agent.ask(prompt)
         response = st.write_stream(stream)
+        print(f"Retrieval agent memory: {retrieval_agent.get_memories()}")
+        print(f"Retrieval agent memory summary: {retrieval_agent.get_memory_summary()}")
 
     st.session_state.messages.append(
         {
